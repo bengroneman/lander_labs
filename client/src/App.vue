46c3afe213bb9255bd2c26f7dd2886cf02b48777
bengroneman <bluegroneman10@gmail.com>
2021-08-08 16:10:58 -0600

    initial setup with navigation bar and scaffolding

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navigation></Navigation>
+  </div>
+  <router-view />
+</template>
+
+<script>
+import Navigation from "@/components/global/Navigation.vue";
+
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
